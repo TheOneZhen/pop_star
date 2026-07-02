31c906f04cd0521e2708ec53c5fcfd00b0cd5126
Z <zhenisbusy@qq.com>
2026-07-02 10:27:57 +0800

    目录结构调整

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'core/theme/app_theme.dart';
 import 'features/home/home_screen.dart';
-import 'features/game/home_screen.dart';
+import 'features/game/game_screen.dart';
+import 'features/game/result_screen.dart';
 
 final _router = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const HomePage()),
     GoRoute(path: '/game', builder: (context, state) => GameScreen()),
+    GoRoute(
+      path: '/game/result',
+      builder: (context, state) => ResultScreen(score: 0),
+    ), // TODO: 将分数保存在游戏状态中并传递分数
   ],
 );
 
